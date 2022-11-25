2a2345a5e3752a579579a31159fc82d1d7ab6a74
Laura Valiente <lauravcruzdaw@gmail.com>
2022-11-25 14:13:28 +0100

    ruta linux

--- autoload.php
+++ autoload.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 function autoload($nombreClase)
 {
     $dir = str_replace("\\", "/", $nombreClase); 
-    include_once $dir . ".php";
+    include_once "../" . $dir . ".php";
 };
 
 spl_autoload_register('autoload');
