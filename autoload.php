a5b94a77b7727bb0fda54a15fc382f26d0ff2e5c
Laura Valiente <lauravcruzdaw@gmail.com>
2022-11-27 22:33:15 +0100

    errores docker 8

--- autoload.php
+++ autoload.php
@@ -1,10 +1,10 @@
 <?php
 
 declare(strict_types=1);
-define('BASE_PATH', realpath(dirname(__FILE__)));
+
 function autoload($nombreClase)
 {
-    $dir = BASE_PATH . '/lib/' . str_replace('\\', '/', $nombreClase) . '.php';
+    $dir = "../" . str_replace('\\', '/', $nombreClase) . '.php';
     echo $dir;
     include_once $dir;
 };
